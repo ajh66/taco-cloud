645155831e0b86b21672266622300bed242833ce
Andy Jiang H <andy.h.jiang@ericsson.com>
2018-11-12 16:20:01 +0800

    Add home page relevant stuff: a controller, a view, and a test case

---
+++ src/main/java/com/ajh/taco/HomeController.java
@@ -0,0 +1,14 @@
+package com.ajh.taco;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	@GetMapping("/")
+	public String home() {
+		return "home"; // This value is interpreted as the logical name of a view.
+					   // How that view is implemented depends on a few factors,
+					   // but because Thymeleaf is in your classpath, you can define that template with Thymeleaf.
+	}
+}
