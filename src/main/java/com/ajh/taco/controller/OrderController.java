7b4f381b3b2d77d78f9aec4d46392f094258c3e8
Andy Jiang H <andy.h.jiang@ericsson.com>
2018-11-15 09:01:10 +0800

    Implement fields validation for order form page.

--- src/main/java/com/ajh/taco/controller/OrderController.java
+++ src/main/java/com/ajh/taco/controller/OrderController.java
@@ -1,7 +1,10 @@
 package com.ajh.taco.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,8 +24,14 @@ public class OrderController {
 	}
 	
 	@PostMapping
-	public String processOrder(Order order) {
+	public String processOrder(@Valid Order order, Errors errors) {
 		log.info("Order submitted: " + order);
+		
+		if (errors.hasErrors()) {
+//			log.info("Error: " + errors);
+			return "orderForm";
+		}
+		
 		return "redirect:/";
 	}
 }
